d616de32b2dd372885bfa6d2aced7bd3b3e85186
hudson <hudson.s.bernardo@hotmail.com>
2024-04-04 11:31:25 -0300

    initial commit

---
+++ Day-01 - Beginner - working with variables in python to manage data/exercise02-debugging practice.py
@@ -0,0 +1,14 @@
+# Look at the code bellow. There are errors on all 4 lines of code.
+# Fix the code so that it runs without errors.
+
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+# Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello" + "world")')
+print("New lines can be created with a backslash and n.")
