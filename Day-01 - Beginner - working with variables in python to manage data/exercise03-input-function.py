f87b5c13ca389798ef825ac74af3e136b1f378b2
hudson <hudson.s.bernardo@hotmail.com>
2024-04-04 11:46:24 -0300

    day 1

---
+++ Day-01 - Beginner - working with variables in python to manage data/exercise03-input-function.py
@@ -0,0 +1,6 @@
+# Write a program that calculates and outputs the number of characters in any name.
+# Your code should work for any name.
+# Your code should only output the number, no other text is needed
+
+name = input("What's your name? ")
+print(len(name))
