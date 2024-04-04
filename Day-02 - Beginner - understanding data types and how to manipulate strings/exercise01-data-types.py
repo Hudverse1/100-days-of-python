024262f1dcb398590b84aa03550fe8fb7c9adc57
hudson <hudson.s.bernardo@hotmail.com>
2024-04-04 12:10:27 -0300

    day 2

---
+++ Day-02 - Beginner - understanding data types and how to manipulate strings/exercise01-data-types.py
@@ -0,0 +1,7 @@
+# Write a program that adds the digits in a 2-digit number.
+# e.g. if the input was 35, then the output should be 3 + 5 = 8
+
+two_digit_number = input("Digit a 2-digit number: ")
+number1 = int(two_digit_number[0])
+number2 = int(two_digit_number[1])
+print(number1 + number2)
